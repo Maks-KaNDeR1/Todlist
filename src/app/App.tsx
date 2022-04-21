3cc5bb2114fe9ec81b8aff6035a395e38c3d0fe8
Maks-KaNDeR <makskander1@gmail.com>
2022-04-21 22:16:56 +0300

    initialized,isLoggedIn, log out

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import { TodolistsList } from '../features/TodolistsList/TodolistsList'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { AppRootStateType } from './store'
-import { RequestStatusType } from './app-reducer'
+import { initializeAppTC, RequestStatusType } from './app-reducer'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -15,6 +15,8 @@ import { Menu } from '@mui/icons-material';
 import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
 import { Login } from '../features/Login/Login'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { CircularProgress } from '@mui/material'
+import { logoutTC } from '../features/Login/auth-reducer'
 
 type PropsType = {
     demo?: boolean
@@ -22,6 +24,27 @@ type PropsType = {
 
 function App({ demo = false }: PropsType) {
     const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    const isInitialized = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.isInitialized)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+
+    
+    const dispatch = useDispatch()
+    useEffect( () => {
+        dispatch(initializeAppTC())
+    })
+ 
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+     }
+
+     let logoutHendler =() => {
+         dispatch(logoutTC())
+     }
+     
+ 
     return (
         <div className="App">
             <ErrorSnackbar />
@@ -33,7 +56,7 @@ function App({ demo = false }: PropsType) {
                     <Typography variant="h6">
                         News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    { isLoggedIn && <Button color="inherit" onClick={logoutHendler} >Log out</Button>}
                 </Toolbar>
                 {status === 'loading' && <LinearProgress />}
             </AppBar>
--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,6 +1,12 @@
+import { Dispatch } from "redux"
+import { authAPI } from "../api/todolists-api"
+import { setIsLoggedInAC } from "../features/Login/auth-reducer"
+import { handleServerAppError, handleServerNetworkError } from "../utils/error-utils"
+
 const initialState: InitialStateType = {
     status: 'idle',
-    error: null
+    error: null,
+    isInitialized: false
 }
 
 export const appReducer = (state: InitialStateType = initialState, action: AppActionsType): InitialStateType => {
@@ -9,6 +15,8 @@ export const appReducer = (state: InitialStateType = initialState, action: AppAc
             return {...state, status: action.status}
         case 'APP/SET-ERROR':
             return {...state, error: action.error}
+        case 'APP/SET-IS-INITIALIZED':
+            return {...state, isInitialized: action.isInitialized}
         default:
             return {...state}
     }
@@ -20,15 +28,36 @@ export type InitialStateType = {
     status: RequestStatusType
     // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
     error: string | null
+    isInitialized: boolean
 }
 
 export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
 export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setAppInitializedAC = (isInitialized: boolean) => ({type: 'APP/SET-IS-INITIALIZED', isInitialized} as const)
+
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+    authAPI.me().then(res => {
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC(true));
+        } else {
+            handleServerAppError(res.data, dispatch);
+        }
+    })
+    .catch((error) => {
+        handleServerNetworkError(error, dispatch)
+    })
+    .finally( () => {
+        dispatch(setAppInitializedAC(true))
+    })
+ }
+
 
 export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
 export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetAppInitializedActionType = ReturnType<typeof setAppInitializedAC>
 
 export type AppActionsType =
     | SetAppErrorActionType
     | SetAppStatusActionType
+    | SetAppInitializedActionType
 
